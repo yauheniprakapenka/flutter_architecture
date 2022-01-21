632ede3991d4aa8b3fbb088e3987aa062caae82b
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-01-21 23:14:29 +0300

    fix: fix incapsulation writer lib

--- modules/features/writer/lib/writer.dart
+++ modules/features/writer/lib/writer.dart
@@ -1,6 +1,3 @@
 library writer;
 
-export 'src/data/repositories/writer_repository/i_writer_repository.dart';
-export 'src/data/repositories/writer_repository/mock_writer_repository/mock_writer_repository.dart';
-export 'src/data/repositories/writer_repository/writer_repository.dart';
 export 'src/ui/pages/writers_list_page.dart';
