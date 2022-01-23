0d2a4ed792bd3a3a9423b5afc8d9beb6e1138f9e
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-01-23 17:16:44 +0300

    [writer] ref: fix mock writer storage

--- modules/features/writer/lib/src/app/di/writer_di.dart
+++ modules/features/writer/lib/src/app/di/writer_di.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import '../../data/repositories/writer_repository/mock_writer_repository/mock_writer_repository.dart';
+import '../../data/storage/mock_local_storage/mock_writer_storage.dart';
 import '../../data/repositories/writer_repository/writer_repository_impl.dart';
 import '../../domain/repositories/writer_repository/i_writer_repository.dart';
 
@@ -8,7 +8,7 @@ class WriterDI {
   static void addDependencies() {
     Get.put<IWriterRepository>(
       WriterRepositoryImpl(
-        localStorage: MockWriterRepository(),
+        localStorage: MockWriterStorage(),
       ),
     );
   }
--- modules/features/writer/lib/src/data/storage/mock_local_storage/mock_writer_storage.dart
+++ modules/features/writer/lib/src/data/storage/mock_local_storage/mock_writer_storage.dart
@@ -1,8 +1,8 @@
-import '../../../../data/dto/writer_dto.dart';
-import '../../../../data/storage/i_writer_storage.dart';
-import '../../../../data/storage/mock_local_storage/writers/writers.dart';
+import '../../dto/writer_dto.dart';
+import '../i_writer_storage.dart';
+import 'writers/writers.dart';
 
-class MockWriterRepository implements IWriterStorage {
+class MockWriterStorage implements IWriterStorage {
   @override
   Future<List<WriterDTO>> getAllWriters() async {
     await Future.delayed(const Duration(seconds: 1));
