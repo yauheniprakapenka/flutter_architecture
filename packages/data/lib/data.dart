b96817088f932fe6228d800a75e1bc8b55afa20e
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-01-30 13:16:22 +0300

    [writer] ref: refactor data layer

--- packages/data/lib/data.dart
+++ packages/data/lib/data.dart
@@ -1,3 +1,4 @@
 library data;
 
-export 'src/services/service_locator.dart';
+export 'src/repositories/writer_repository/writer_repository_impl.dart';
+export 'src/storage/mock_writer_storage/mock_writer_storage.dart';
--- packages/data/lib/src/repositories/writer_repository/writer_repository_impl.dart
+++ packages/data/lib/src/repositories/writer_repository/writer_repository_impl.dart
@@ -4,15 +4,15 @@ import '../../mappers/writer_mapper.dart';
 import '../../storage/i_writer_storage.dart';
 
 class WriterRepositoryImpl implements IWriterRepository {
-  final IWriterStorage _localStorage;
+  final IWriterStorage _localDataSource;
 
   const WriterRepositoryImpl({
-    required IWriterStorage localStorage,
-  }) : _localStorage = localStorage;
+    required IWriterStorage localDataSource,
+  })  : _localDataSource = localDataSource;
 
   @override
   Future<List<Writer>> getAllWriters() async {
-    final writersDtoList = await _localStorage.getAllWriters();
+    final writersDtoList = await _localDataSource.getAllWriters();
     return writersDtoList.map(WriterMapper.mapDtoToEntity).toList();
   }
 }
