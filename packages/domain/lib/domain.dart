6e75d92946f9d5d5ac39925c4f75d144ee3d3966
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-05 21:49:08 +0300

    ref: refactor domain

--- packages/domain/lib/domain.dart
+++ packages/domain/lib/domain.dart
@@ -1,7 +1,7 @@
 library domain;
 
-export 'src/bloc/writer_bloc/writer_bloc.dart';
-export 'src/bloc/writer_bloc/writer_events/writer_events.dart';
-export 'src/bloc/writer_bloc/writer_state.dart';
 export 'src/entities/entities.dart';
+export 'src/providers/writer_provider/events/writer_events.dart';
+export 'src/providers/writer_provider/writer_bloc.dart';
+export 'src/providers/writer_provider/writer_state.dart';
 export 'src/repositories/i_writer_repository.dart';
